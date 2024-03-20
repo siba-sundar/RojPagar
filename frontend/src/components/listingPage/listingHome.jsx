fa9dcb75b66290678a6d99f4750a38da828bcd20
Siba Sundar <siba.edu2003@gmail.com>
2024-03-21 04:57:01 +0530

    signup page

---
+++ frontend/src/components/listingPage/listingHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./listingHome.css"
+import ListingNavBar from "./listingNavBar.jsx"
+
+function ListingHome() {
+  return (
+    <>
+      <ListingNavBar/>
+    </>
+  )
+}
+
+export default ListingHome
