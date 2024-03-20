ec7597ab3e460c82cbc7199e640ab171ffbcd0b7
Saksham Shrey <sakshamshrey_work@icloud.com>
2024-03-20 23:07:20 +0530

    added joblisting routes

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,4 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = 3000
-const connectToDatabase = require('./database/connection')
-connectToDatabase()
+// index.js
 
-app.use(cors())
-app.use(express.json())
+const app = require('./server.js');
 
-app.get('/', (req, res) => {
-    res.send('<center><h1>Welcome to Roj Pagar Backend</h1>' +
-        '<h3><a href="https://github.com/Aryan-Sheregar/RojPagar.git" target="_blank">Visit Repository</a></h3></center>')
-})
-
-
-app.listen(port, () => {
-    console.log(`RojPagar Backend listening on http://localhost:${port}`)
-})
--- frontend/src/components/listingPage/jobCard.jsx
+++ frontend/src/components/listingPage/jobCard.jsx
@@ -44,3 +44,5 @@ function JobCard(){
 }
 
 export default JobCard
+
+
