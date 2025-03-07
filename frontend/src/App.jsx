43897c45eee131972a26fefa2d277c56009a5a74
Siba Sundar <siba.edu2003@gmail.com>
2025-03-07 23:51:33 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,43 @@ import { useState } from 'react'
 import './App.css'
 import JobCard from "./components/loginSignup/signup"
 
+import Layout from './components/layout.jsx';
+import Home from './components/landingPage/landingHome.jsx';
+import AboutUs from './components/landingPage/landingAbout.jsx';
+import Contacts from './components/landingPage/landingContact.jsx';
+import LoginPage from "./components/loginSignup/loginPage.jsx";
+import Signup from './components/loginSignup/signup.jsx';
+import ListingHome from './components/listingPage/listingHome.jsx';
+import JobDetails from './components/listingPage/jobDetails.jsx';
+
+
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 function App() {
   const [count, setCount] = useState(0)
 
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "aboutus", element: <AboutUs /> },
+        { path: "contacts", element: <Contacts /> }
+      ]
+    },
+    { path: "login", element: <LoginPage /> },
+    { path: "signup", element: <Signup /> },
+    { path: "contacts", element: <Contacts /> },
+    { path: "listinghome", element: <ListingHome /> },
+    { path: "jobdetails", element: <JobDetails /> }
+  ]);
+
   return (
     <>
-    <JobCard/>
+    <RouterProvider router={router} />
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,31 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-//import App from './App.jsx';
-import './index.css';
-import Layout from './components/layout.jsx';
-import Home from './components/landingPage/landingHome.jsx';
-import AboutUs from './components/landingPage/landingAbout.jsx';
-import Contacts from './components/landingPage/landingContact.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from "./components/loginSignup/loginPage.jsx";
-import Signup from './components/loginSignup/signup.jsx';
-import ListingHome from './components/listingPage/listingHome.jsx'
-import JobDetails from './components/listingPage/jobDetails.jsx';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<Home />} />
-          <Route path="aboutus" element={<AboutUs />} />
-          <Route path="contacts" element={<Contacts />} />
-        </Route>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="contacts" element={<Contacts />} />
-        <Route path="listinghome" element={<ListingHome />} />
-        <Route path="jobdetails" element={<JobDetails />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
